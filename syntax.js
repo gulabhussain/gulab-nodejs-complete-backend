711c0ce0c1c7aab0c11aaeebbb5dd2bb37dcca92
Gulab Hussain <gulabhussain2033@gmail.com>
2026-01-21 23:46:00 +0530

    Delete syntax.js

--- syntax.js
+++
@@ -1,6 +0,0 @@
-const testingSyntax = () =>{
-  console.log("i am inside testing syntax");  // no error
-  console.log("i am inside testing syntax"    // error occur
-};
-
-module.exports = testingSyntax;
